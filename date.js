f939d8f5ce2933c3ef9e6b01de274ccc621d1585
Ijrini17 <isporova@yandex.ru>
2022-02-18 10:41:08 +0300

    Update date.js

--- date.js
+++ date.js
@@ -1,13 +1,14 @@
 // задача про пятницы 13ое
 
-let date1 = new Date(2000, 00, 14);
+let date1 = new Date(Date.UTC(2000, 00, 13));
 let date2 = new Date();
 console.log(date1)
 console.log(date2)
 let number = 0;
 
 for (date1; date1 < date2; date1.setMonth(date1.getMonth() + 1)) {
-    if (date1.getDay() == 6) {
+
+    if (date1.getDay() === 5) {
         number += 1
         console.log(date1)
     }
