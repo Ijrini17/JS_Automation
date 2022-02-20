ece6096b06c33398e8bccf2e5ff9587edf49ae2f
Ijrini17 <isporova@yandex.ru>
2022-02-20 21:27:21 +0300

    Update div2.js

--- div2.js
+++ div2.js
@@ -3,13 +3,13 @@
 let n = 18 //число
 let div = 5 //на сколько частей поделить
 let a = []
-let rand = function (min, max) {
-    let b = Number((Math.random() * (max - min + 1) + min).toFixed(2))
+let rand = function (max) {
+    let b = Number((Math.random() * max).toFixed(2))
     return b
 }
 let func2 = function () {
     for (i = 1; i < div; i++) {
-        let c = rand(0, n -1)
+        let c = rand(n)
         a.push(c)
         n -= c
     }
